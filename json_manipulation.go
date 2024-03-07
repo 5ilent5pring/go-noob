78bdba61aa865e334303b50bc44b29a12bc55d3b
5ilent5pring <162632271+5ilent5pring@users.noreply.github.com>
2024-03-08 00:31:54 +0300

    Update json_manipulation.go

--- json_manipulation.go
+++ json_manipulation.go
@@ -1,3 +1,30 @@
+/*
+[
+	{"name": "David", "age": 28},
+	{"name": "Eva", "age": 35},
+	{"name": "Frank", "age": 22},
+	{"name": "Grace", "age": 27},
+	{"name": "Henry", "age": 31},
+	{"name": "Ivy", "age": 23},
+	{"name": "Jack", "age": 29},
+	{"name": "Kelly", "age": 26},
+	{"name": "Liam", "age": 33},
+	{"name": "Mia", "age": 24}
+]
+
+Suppose we have a 'data.json' like this, and after executing the code, we get the below response:
+
+People under 30:
+Name: Frank, Age: 22
+Name: Ivy, Age: 23
+Name: Mia, Age: 24
+Name: Kelly, Age: 26
+Name: Grace, Age: 27
+Name: David, Age: 28
+Name: Jack, Age: 29
+
+*/
+
 package main
 
 import (
